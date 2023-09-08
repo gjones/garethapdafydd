4cc4adb55b79b0924fd71a97eb00a470b96a5eac
Gareth Jones <github@garethdjones.com>
2023-09-07 21:00:29 -0400

    Adding a delay

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import React, { useState } from 'react'
 import useDarkMode from 'use-dark-mode'
 import { ThemeProvider } from 'styled-components'
 import { lightTheme, darkTheme } from '../components/theme'
@@ -6,12 +7,17 @@ import { lightTheme, darkTheme } from '../components/theme'
 import '@styles/helpers/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { value: isDarkMode } = useDarkMode(true)
-  const theme = isDarkMode ? darkTheme : lightTheme
+  const [isLoading, setIsLoading] = useState(true) // Add a loading state
+  const darkMode = useDarkMode(true)
+  const theme = darkMode.value ? darkTheme : lightTheme
+
+  setTimeout(() => {
+    setIsLoading(false)
+  }, 1000)
 
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      {isLoading ? null : <Component {...pageProps} />}
     </ThemeProvider>
   )
 }
