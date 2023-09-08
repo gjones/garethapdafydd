5db66a2b5e9a336a4d8d3fbc60f11e9a37416951
Gareth Jones <github@garethdjones.com>
2023-09-07 21:25:18 -0400

    Back to basic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app'
-import React, { useState } from 'react'
 import useDarkMode from 'use-dark-mode'
 import { ThemeProvider } from 'styled-components'
 import { lightTheme, darkTheme } from '../components/theme'
@@ -7,17 +6,12 @@ import { lightTheme, darkTheme } from '../components/theme'
 import '@styles/helpers/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isLoading, setIsLoading] = useState(true) // Add a loading state
-  const darkMode = useDarkMode(true)
-  const theme = darkMode.value ? darkTheme : lightTheme
-
-  setTimeout(() => {
-    setIsLoading(false)
-  }, 1000)
+  const { value: isDarkMode } = useDarkMode(true)
+  const theme = isDarkMode ? darkTheme : lightTheme
 
   return (
     <ThemeProvider theme={theme}>
-      {isLoading ? null : <Component {...pageProps} />}
+      <Component {...pageProps} />
     </ThemeProvider>
   )
 }
